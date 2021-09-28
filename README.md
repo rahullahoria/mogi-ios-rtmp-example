518e10d0f60d2581b4dd2d595c994b81e6a24f43
Rajnish Panwar <rajnishp_pawar90@yahoo.com>
2021-09-28 15:12:49 +0530

    adding public

--- README.md
+++ README.md
@@ -6,8 +6,15 @@ Step 1:
 git clone https://github.com/rahullahoria/mogi-ios-rtmp-example.git
 
 Step 2:
+
 open the project with xcode
+open rtmp.xcodeproj
 
 Step 3:
 
-run the proeject
+run the project
+
+Step 4:
+
+Open app and follow as below image urls
+
--- rtmp/Preference.swift
+++ rtmp/Preference.swift
@@ -1,6 +1,6 @@
 struct Preference {
     static var defaultInstance = Preference()
 
-    var uri: String? = "rtmp://15.207.119.231/live/"
-    var streamName: String? = "6140b007ac5df836e460f484"
+    var uri: String? = "rtmp://live-stream.mogiio.com/live/"
+    var streamName: String? = "6152ac7f1c399b4909a74bd5"
 }
